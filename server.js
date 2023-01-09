f122d8abcbea9064f3ecae3dedda725b9153afeb
Andrew Connally <ajconnally93@gmail.com>
2023-01-09 13:35:22 -0600

    set up server.js file, added npm dependencies

---
+++ server.js
@@ -0,0 +1,49 @@
+// setting up middleware
+
+
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+
+// local port
+const PORT = process.env.PORT || 3001;
+
+
+// sets up handlebars with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: "I'VE GOT A SECRET",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// tells express.js which template engine we will use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
