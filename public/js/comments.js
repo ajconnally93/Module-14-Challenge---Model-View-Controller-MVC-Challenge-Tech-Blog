7f3d48a39c50ba00781055b94d50fa496c2e1b23
Andrew Connally <ajconnally93@gmail.com>
2023-01-25 14:40:53 -0600

    added content to README. fixed small bug with adding comments to blog

--- public/js/comments.js
+++ public/js/comments.js
@@ -3,18 +3,18 @@ const commentHandler = async function (event) {
 	event.preventDefault();
 
 	const blog = document.querySelector('.new-comment-form').dataset.blogid;
-	const commentDesc = document.querySelector('#comment_description')
+	const comment_description = document.querySelector('#comment_description').value.trim();
 
 
     // allows for creation/POST of comments by user
     // may need to require withAuth to attach to user profile? can test later
-	if (commentDesc) {
+	if (comment_description) {
 
 		await fetch('/api/comments', {
 			method: 'POST',
 			body: JSON.stringify({
 				blog,
-				commentDesc,
+				comment_description,
 			}),
 
 			headers: {
